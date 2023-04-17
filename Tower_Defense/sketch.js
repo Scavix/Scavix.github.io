987b04b4b864b4bc0d040a917a852f28536a632e
Scav <42140533+Scavix@users.noreply.github.com>
2023-04-17 15:55:23 +0200

    update

--- Tower_Defense/sketch.js
+++ Tower_Defense/sketch.js
@@ -1,21 +1,40 @@
 let myPoints = [],
-    myMap,
-    pickTurrets = [],
-    turretsColour = ["red", "blue", "green", "yellow"],
-    costs = [],
-    enemies = [],
-    coins = 0,
-    turretMode = "red",
-    myOverlay,
-    enemiesKilled = 0,
-    projectiles = [],
-    difficulty = [1, 2, 3];
+  myMap,
+  pickTurrets = [],
+  turretsColour = ["red", "blue", "green", "yellow"],
+  costs = [],
+  enemies = [],
+  coins = 0,
+  turretMode = "red",
+  myOverlay,
+  enemiesKilled = 0,
+  projectiles = [],
+  difficulty = [1, 2, 3],
+  redImg,
+  blueImg,
+  greenImg,
+  yellowImg,
+  enemyImg,
+  images = [];
+
+function preload() {
+  redImg = loadImage('assets/red.png');
+  blueImg = loadImage('assets/blue.png');
+  greenImg = loadImage('assets/green.png');
+  yellowImg = loadImage('assets/yellow.png');
+  enemyImg = loadImage('assets/enemy.png');
+}
 
 function setup() {
   createCanvas(
     windowWidth < windowHeight ? windowWidth : windowHeight,
     windowWidth < windowHeight ? windowWidth : windowHeight
   );
+  images.push(redImg);
+  images.push(blueImg);
+  images.push(greenImg);
+  images.push(yellowImg);
+  images.push(enemyImg);
   myMap = new GameMap();
   myMap.gamePath.addPoint(new GamePoint(width / 10, 0));
   myMap.gamePath.addPoint(new GamePoint(width / 10, height / 10 * 9));
@@ -37,10 +56,10 @@ function setup() {
   myMap.gamePath.addTowerPoint(new GameTowerPoint(width / 5 * 4, height / 10 * 7));
   for (let i = 0; i < 4; i++) {
     pickTurrets.push(new GameTower(width / 2 + (i + 1) * width / 10, width / 10 * 9, width / 20, turretsColour[i]));
-    costs.push(this.cost = Math.floor(random(i * difficulty[0] * 10, (i + 1) * difficulty[0] * 10)));
+    costs.push(this.cost = Math.floor(random(i * difficulty[0] * 5 + 1, (i + 1) * difficulty[0] * 5)));
   }
   addOverlay(pickTurrets[0].x, pickTurrets[0].y);
-  enemies.push(new GameEnemy(width / 10, 0, width / 20, width / 10, myMap.gamePath));
+  enemies.push(new GameEnemy(myMap.gamePath));
 }
 
 function draw() {
@@ -60,6 +79,25 @@ function draw() {
     projectiles[i].show();
     projectiles[i].move();
   }
+  myMap.shot();
+  cleanProjectiles();
+  cleanEnemies();
+}
+
+function cleanProjectiles() {
+  for (let i = 0; i < projectiles.length; i++) {
+    if (projectiles[i].x > width || projectiles[i].x < 0 || projectiles[i].y > height || projectiles[i].y < 0) {
+      projectiles.splice(i, 1);
+    }
+  }
+}
+
+function cleanEnemies() {
+  for (let i = 0; i < enemies.length; i++) {
+    if (enemies[i].x > width || enemies[i].x < 0 || enemies[i].y > height || enemies[i].y < 0) {
+      enemies.splice(i, 1);
+    }
+  }
 }
 
 class GameTower {
@@ -68,16 +106,17 @@ class GameTower {
     this.y = y;
     this.s = s;
     this.type = type;
-    this.range = width / 10;
+    this.range = width / 4;
   }
   show() {
     fill(this.type);
     square(this.x, this.y, this.s);
+    image(images[turretsColour.indexOf(this.type)], this.x, this.y, this.s, this.s);
   }
   shotEnemy() {
     for (let i = 0; i < enemies.length; i++) {
       if (dist(this.x, this.y, enemies[i].x, enemies[i].y) < this.range) {
-        projectiles.push(new Projectile(this.x, this.y, enemies[i].x, enemies[i].y, this.type));
+        projectiles.push(new Projectile(this.x + this.s / 2, this.y + this.s / 2, enemies[i].x, enemies[i].y, this.type));
       }
     }
   }
@@ -95,6 +134,11 @@ class GameMap {
     this.gamePath.show();
     for (let i = 0; i < this.towers.length; i++) {
       this.towers[i].show();
+    }
+  }
+  shot() {
+    for (let i = 0; i < this.towers.length; i++) {
+      this.towers[i].shotEnemy();
     }
   }
 }
@@ -215,36 +259,77 @@ function showCosts() {
 }
 
 class GameEnemy {
-  constructor(x, y, path) {
-    this.x = x;
-    this.y = y;
+  constructor(path) {
+    this.point = 0;
+    this.x = path.points[this.point].x;
+    this.y = path.points[this.point].y;
     this.life = enemiesKilled * Math.floor(random(5)) + 1;
     this.path = path;
+    this.speed = width / 250 * difficulty[0];
+    this.toRemove = false;
   }
   show() {
-    fill(0);
-    circle(this.x, this.y, width / 50);
+    if (!this.toRemove) {
+      fill('white');
+      circle(this.x, this.y, width / 20);
+      image(images[4], this.x - width / 20 / 2, this.y - width / 20 / 2, width / 20, width / 20);
+    }
   }
   move() {
-    //this.x += 
+    if (this.toRemove) {
+      return;
+    }
+    let xDist = this.path.points[this.point + 1].x - this.x;
+    let yDist = this.path.points[this.point + 1].y - this.y;
+    this.x += (xDist < this.speed && xDist >= 0) ? 0 : (xDist < this.speed && xDist < 0) ? -this.speed : this.speed;
+    this.y += (yDist < this.speed && yDist >= 0) ? 0 : (yDist < this.speed && yDist < 0) ? -this.speed : this.speed;
+    this.check();
+  }
+  check() {
+    let xDist = this.path.points[this.point + 1].x - this.x;
+    let yDist = this.path.points[this.point + 1].y - this.y;
+    if (
+      (abs(xDist) < this.speed)
+      &&
+      (abs(yDist) < this.speed)) {
+      if (this.point + 1 == this.path.points.length - 1) {
+        this.toRemove = true;
+        enemiesKilled++;
+        enemies.push(new GameEnemy(myMap.gamePath));
+        return;
+      }
+      this.x = this.path.points[this.point + 1].x;
+      this.y = this.path.points[this.point + 1].y;
+      this.point++;
+    }
   }
 }
 
 class Projectile {
-  constructor(x, y, x2, y2) {
+  constructor(x, y, x2, y2, type) {
     this.x = x;
     this.y = y;
     this.x2 = x2;
     this.y2 = y2;
-    this.speed = 10;
+    this.speed = 5;
     this.angle = atan2(y2 - y, x2 - x);
+    this.type = type;
+    this.damage = difficulty[0];
   }
   show() {
-    fill(this.type);
-    circle(this.x, this.y, width / 50);
+    if (!this.toRemove) {
+      fill(this.type);
+      circle(this.x, this.y, width / 50);
+    }
   }
   move() {
+    if (this.toRemove) {
+      return;
+    }
     this.x += this.speed * cos(this.angle);
     this.y += this.speed * sin(this.angle);
+    if (this.x > width || this.x < 0 || this.y > height || this.y < 0) {
+      this.toRemove = true;
+    }
   }
 }
