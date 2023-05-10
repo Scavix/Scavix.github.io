ad71b43588486bce9838458c721bede53bd4005c
Scav <42140533+Scavix@users.noreply.github.com>
2023-05-10 10:11:20 +0200

    update

---
+++ an_old_camera_story/py.py
@@ -0,0 +1,17 @@
+directory1 = "photos/12_04_2023/"
+directory2 = "photos/14_04_2023/"
+directory3 = "photos/18_04_2023/"
+directory4 = "photos/09_05_2023/"
+fileExtension = ".JPG"
+print("--------------------")
+for i in range(3571, 3604):
+    print('"'+directory1+"100_" + str(i) + fileExtension+'",')
+print("--------------------")
+for i in range(3612, 3628):
+    print('"'+directory2+"100_" + str(i) + fileExtension+'",')
+print("--------------------")
+for i in range(3631, 3659):
+    print('"'+directory3+"100_" + str(i) + fileExtension+'",')
+print("--------------------")
+for i in range(3828, 3859):
+    print('"'+directory4+"100_" + str(i) + fileExtension+'",')
