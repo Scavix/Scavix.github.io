05e181d800e7c50dba6cf186226b89f69cc852a2
Scav <42140533+Scavix@users.noreply.github.com>
2023-10-10 02:22:40 +0200

    Update helper_links.py

--- helper_links.py
+++ helper_links.py
@@ -2,26 +2,43 @@ import requests
 import PySimpleGUI as sg
 from bs4 import BeautifulSoup
 from datetime import datetime
+import os.path
 
 def main():
     outstr = ""
-    layout = [  [sg.Text('Enter URL'), sg.InputText(key='-URL-', default_text='isnert URL here')],
-                [sg.Multiline(default_text="", expand_x=True, expand_y=True, key='-OUTPUT-')],
-                [sg.Button('Ok'), sg.Button('Cancel'), sg.Button('Generate Source')]]
+    try:
+        if os.path.isfile("cache.tmp"):
+            f = open("cache.tmp", "r")
+            outstr = f.read()
+            f.close()
+            os.remove("cache.tmp")
+    except Exception as e:
+        print(e)
+        pass
+    layout = [  [sg.Text('Enter URL'), sg.InputText(key='-URL-', default_text='insert URL here')],
+                [sg.Multiline(expand_x=True, expand_y=True, key='-OUTPUT-', default_text=outstr)],
+                [sg.Button('Add'), sg.Button('Generate Source'), sg.Button('Exit and Save'), sg.Button('Exit')]]
 
     window = sg.Window('Url generate', layout, element_justification='c', finalize=True, size=(600, 200))
 
     try:
         while True:
             event, values = window.read()
-            if event == sg.WIN_CLOSED or event == 'Cancel':
-                    break
+            if event == sg.WIN_CLOSED or event == 'Exit and Save':
+                break
+            if event == 'Exit':
+                os.remove("cache.tmp")
+                break
             elif event == 'Generate Source':
                 response = requests.get("https://scavix.github.io/helper_links.py")
-                f = open("helper_links.py", "w")
-                f.write(response.text)
-                f.close()
-            elif event == 'Ok':
+                if response.status_code == 200:
+                    f = open("helper_links.py", "w")
+                    f.write(response.text)
+                    f.close()
+                    sg.popup("Done")
+                else:
+                    sg.popup("Web site does not exist or is not reachable")
+            elif event == 'Add':
                 url=window['-URL-'].get()
                 headers = {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
@@ -38,16 +55,21 @@ def main():
                         soup = BeautifulSoup(response.content, 'html.parser')
                         title = soup.title.string
                         outstr += "<li><a href = \"" + str(url).strip() + "\">" + str(title) + "</a></li>\n"
+                        save_to("cache.tmp",outstr)
                 else:
                     sg.popup("Web site does not exist or is not reachable")
             
                 window['-OUTPUT-'].update(value=outstr)
                 window['-URL-'].update(value="")
     except:
-        f = open(str(datetime.now()+"txt"), "w")
-        f.write(outstr)
-        f.close()
+        save_to("cache.tmp", outstr)        
+        sg.popup("Found exception, cache saved")
     window.close()
+    
+def save_to(dir,myStr):
+    f = open(dir, "w")
+    f.write(myStr)
+    f.close()
 
 if __name__ == "__main__":
     main()
